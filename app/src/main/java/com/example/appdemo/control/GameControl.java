402a833f920d034251a652dd32752944d76948d7
lizeyang <lzydq2012@163.com>
2017-11-08 21:30:00 +0800

    定义了查询是否有可消去方块的方法

--- app/src/main/java/com/example/appdemo/control/GameControl.java
+++ app/src/main/java/com/example/appdemo/control/GameControl.java
@@ -7,5 +7,6 @@ package com.example.appdemo.control;
 public interface GameControl {
     void initNewGame();
     int[] getItemIds();
+    int[] eliminate();
     int[] eliminate(int position1, int position2);
 }
--- app/src/main/java/com/example/appdemo/control/GameControlImpl.java
+++ app/src/main/java/com/example/appdemo/control/GameControlImpl.java
@@ -38,16 +38,35 @@ public class GameControlImpl implements GameControl {
     }
 
     @Override
-    public int[] eliminate(int position1, int position2) {
+    public int[] eliminate() {
         return new int[0];
     }
 
-    protected boolean isAdjacent(int position1, int position2) {
+    @Override
+    public int[] eliminate(int position1, int position2) {
+        int[] list = new int[0];
         int x1, y1, x2, y2;
         x1 = position1 % columnCount;
         y1 = position1 / columnCount;
         x2 = position2 % columnCount;
         y2 = position2 / columnCount;
+        if(isAdjacent(x1, y1, x2, y2)) {
+            changeBlock(x1, y1, x2, y2);
+            list = eliminate();
+            if(list.length == 0) {
+                changeBlock(x1, y1, x2, y2);
+            }
+        }
+        return list;
+    }
+
+    protected void changeBlock(int x1, int y1, int x2, int y2) {
+        int temp = gridData[x1][y1];
+        gridData[x1][y1] = gridData[x2][y2];
+        gridData[x2][y2] = temp;
+    }
+
+    protected boolean isAdjacent(int x1, int y1, int x2, int y2) {
         if ((x1 == x2 && Math.abs(y1 - y2) == 1) || (Math.abs(x1 - x2) == 1 && y1 == y2)) {
             return true;
         }
